c05c394dffe4528f96a38cb1894f7821b8e65123
riisonriis <mulpole@emaili.ee>
2023-03-14 19:33:31 +0200

    asjad töötavad paremini

--- sql.py
+++ sql.py
@@ -1,3 +1,6 @@
+import hashlib
+
+import bcrypt as bcrypt
 from mysql import connector
 from mysql.connector import errorcode # https://dev.mysql.com/doc/connector-python/en/connector-python-example-connecting.html
 import debug
@@ -7,7 +10,7 @@ log = Logija.log
 
 VAJALIKUD_TABELID = [ 'kasutajad' ]
 LOO_TABEL = {
-    "kasutajad": "CREATE TABLE kasutajad ( id serial primary key not null, kasutajanimi varchar( 32 ) not null, parool varchar( 256 ) not null, api-võti varchar( 128 ) not null )"
+    "kasutajad": "CREATE TABLE kasutajad ( id serial primary key not null, kasutajanimi varchar( 32 ) not null, parool varchar( 256 ) not null, api_voti varchar( 128 ) not null )"
 }
 
 class Andmebaas:
@@ -24,19 +27,20 @@ class Andmebaas:
 
         vajalikud_tabelid = self.test_tabelid( )
         for tabeli_nimi in vajalikud_tabelid:
+            print( tabeli_nimi )
             self.loo_tabel( tabeli_nimi )
 
     def loo_andmebaas( self ):
         try:
-            # ei kasuta self.ühenda( ) sest meil on vaja luua andmebaas.
-            ühendus = connector.connect(
+            # ei kasuta self.uhenda( ) sest meil on vaja luua andmebaas.
+            uhendus = connector.connect(
                 user=self.kasutajanimi,
                 password=self.parool,
                 host=self.host
             )
 
-            looja = ühendus.cursor( )
-            looja.execute( f'CREATE DATABASE { self.andmebaas }' )
+            looja = uhendus.cursor( )
+            looja.execute( f'CREATE DATABASE %(andmebaas)s', { 'andmebaas': self.andmebaas } )
             log( f'Loodi andmebaas { self.andmebaas }' )
 
             looja.close( )
@@ -44,9 +48,9 @@ class Andmebaas:
             if error.errno == errorcode.CR_CONN_HOST_ERROR:
                 print( 'Host\'i ei leitud.' )
             elif error.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-                print( 'Ei saanud andmebaasiga ühendust luua. Palun kontrolli oma kasutaja andmed üle.' )
+                print( 'Ei saanud andmebaasiga uhendust luua. Palun kontrolli oma kasutaja andmed ule.' )
             else:
-                print( f'Ei saanud andmebaasiga ühendust luua.\n{ error }' )
+                print( f'Ei saanud andmebaasiga uhendust luua.\n{ error }' )
             exit( )
 
     def loo_tabel( self, tabeli_nimi ):
@@ -54,76 +58,111 @@ class Andmebaas:
             print( 'Tabeli nimi ei ole vajalike tabelite hulgas.' )
             exit( )
 
-        ühendus = self.ühenda( )
+        uhendus = self.uhenda( )
 
-        looja = ühendus.cursor( )
-        looja.execute( f'CREATE TABLE { LOO_TABEL[ tabeli_nimi ] }' )
+        looja = uhendus.cursor( )
+        looja.execute( LOO_TABEL[ tabeli_nimi ] )
         looja.close( )
-        log( f'Loodi tabel { tabeli_nimi } käsuga { LOO_TABEL[ tabeli_nimi ] }' )
+        log( f'Loodi tabel { tabeli_nimi } kasuga { LOO_TABEL[ tabeli_nimi ] }' )
 
-    def ühenda( self ):
-        log( f'{ self.kasutajanimi }({ len( self.parool ) }) ühendab andmebaasiga { self.andmebaas }({ self.host })' )
+    def uhenda( self ):
+        log( f'{ self.kasutajanimi }({ len( self.parool ) }) uhendab andmebaasiga { self.andmebaas }({ self.host })' )
         return connector.connect(
             user     = self.kasutajanimi,
             password = self.parool,
             host     = self.host,
-            database = self.andmebaas
+            database = self.andmebaas,
         )
 
     @staticmethod
-    def leia( ühendus, tabel: str, väli: str, väärtus ):
-        otsija = ühendus.cursor( )
+    def leia( uhendus, tabel: str, vali: str, vaartus ) -> list:
+        otsija = uhendus.cursor( )
 
-        log( f'Otsitakse { tabel }->{ väli } väärtust { väärtus }.' )
-        otsija.execute( f'SELECT { väli } FROM { tabel } WHERE { väli } = { väärtus }' )
+        log( f'Otsitakse { tabel }->{ vali } vaartust { vaartus }.' )
+        data = {
+            'vali': vali,
+            'tabel': tabel,
+            'vaartus': vaartus
+        }
+        otsija.execute( f'SELECT %(vali)s FROM %(tabel)s WHERE %(vali)s = %(vaartus)s', data )
 
-        leitud = [ ]
-        for value in otsija:
-            leitud.append( value )
+        leitud = otsija.fetchall( )
 
         otsija.close( )
 
-        log( f'Tagastati { len( leitud ) } välja.' )
+        log( f'Tagastati { len( leitud ) } valja.' )
         return leitud
 
     def test( self ) -> bool:
-        log( f'{ self.kasutajanimi }({ len( self.parool ) }) testib ühendust andmebaasiga { self.andmebaas }({ self.host }). ' )
+        log( f'{ self.kasutajanimi }({ len( self.parool ) }) testib uhendust andmebaasiga { self.andmebaas }({ self.host }). ' )
         try:
-            self.ühenda( )
+            self.uhenda( )
         except connector.Error as error:
             if error.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-                print( 'Ei saanud andmebaasiga ühendust luua. Palun kontrolli oma kasutaja andmed üle.' )
+                print( 'Ei saanud andmebaasiga uhendust luua. Palun kontrolli oma kasutaja andmed ule.' )
             elif error.errno == errorcode.ER_DATABASE_NAME:
                 print( f'Andmebaasi "{ self.andmebaas }" ei leitud.' )
             else:
-                print(f'Ei saanud andmebaasiga ühendust luua (error #{ error.errno }).')
+                print(f'Ei saanud andmebaasiga uhendust luua (error #{ error.errno }).')
             return True
 
-        log( 'Ühendus loodi edukalt.' )
+        log( 'uhendus loodi edukalt.' )
         return False
 
     def test_tabelid( self ) -> list:
         vajalikud_tabelid = [ ]
         try:
-            ühendus = self.ühenda()
+            uhendus = self.uhenda()
             for tabeli_nimi in VAJALIKUD_TABELID:
-                    otsija = ühendus.cursor()
-                    otsija.execute(f'SHOW TABLES LIKE "{tabeli_nimi}"')
+                    otsija = uhendus.cursor()
+                    otsija.execute(f'SHOW TABLES LIKE %(tabel)s', { 'tabel': tabeli_nimi } )
 
-                    if otsija.rowcount == 0:
+                    if len( otsija.fetchall( ) ) <= 0:
                         vajalikud_tabelid.append( tabeli_nimi )
         except Exception as e:
             print( e )
             exit( )
 
-        log( f'Tabeleid { ", ".join( vajalikud_tabelid ) } ei leitud.' )
-        return vajalikud_tabelid
+        if len( vajalikud_tabelid ) > 0:
+            log( f'Tabeleid { ", ".join( vajalikud_tabelid ) } ei leitud.' )
+            return vajalikud_tabelid
+        else:
+            log( f'Vajalikud tabelid on juba loodud. { VAJALIKUD_TABELID }' )
+            return [ ]
 
-    def user_exists( self, kasutajanimi ) -> bool:
-        ühendus = self.ühenda( )
+    def kas_kasutaja_on_olemas( self, kasutajanimi ) -> bool:
+        uhendus = self.uhenda( )
 
-        leitud = self.leia( ühendus, 'kasutajad', 'kasutajanimi', kasutajanimi )
+        leitud = self.leia( uhendus, 'kasutajad', 'kasutajanimi', kasutajanimi )
 
         print( leitud )
 
-        return False
+        return len( leitud ) == 1
+
+    def loo_kasutaja( self, kasutajanimi, plaintext_parool ) -> bool:
+        # Eeldame, et kasutaja olemasolu on juba kontrollitud
+        # Samuti on kontrollitud ka parooli tugevus
+        api_voti = bcrypt.gensalt( ).decode( 'utf-8' )
+
+        md5 = hashlib.md5( )
+        md5.update( plaintext_parool.encode( 'utf-8' ) )
+
+        parool = md5.hexdigest( )
+
+        uhendus = self.uhenda( )
+
+        sisestaja = uhendus.cursor( )
+
+        sisestaja.execute( 'INSERT INTO kasutajad ( kasutajanimi, parool, api_voti ) VALUES ( %s, %s, %s )', ( kasutajanimi, parool, api_voti ) )
+        log( f'kasutaja { kasutajanimi } loodi edukalt' )
+        uhendus.close( )
+
+        return self.kas_kasutaja_on_olemas( kasutajanimi )
+
+    def kuva_kasutajad( self ) -> None:
+        uhendus = self.uhenda( )
+
+        otsija = uhendus.cursor( )
+        otsija.execute( 'SELECT kasutajanimi FROM kasutajad' )
+
+        print( otsija.fetchall( ) )
--- website.py
+++ website.py
@@ -1,4 +1,14 @@
 from sql import Andmebaas
+from flask import Flask
 
-a = Andmebaas( 'a', 'b', 'c', 'd' )
+Veebileht = Flask( __name__ )
+Andmebaas = Andmebaas( 'root', '', 'sharex_site', 'localhost' )
 
+Andmebaas.kuva_kasutajad( )
+
+@Veebileht.route('/registreeri')
+def registreeri( ):
+    pass
+
+if __name__ == "__main__":
+    Veebileht.run( debug=True )
